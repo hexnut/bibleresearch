479100f99cea5ca433505abe6a04854c7188b322
Steve C <jsec71@yahoo.com>
2025-11-14 14:46:05 -0600

    added divtest and fibonacci

--- bin/divtest.py
+++ bin/divtest.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""Division test """
 
 import sys
 
 if len(sys.argv) > 1:
-    divisor = int(sys.argv[1])
+    DIV = int(sys.argv[1])
 else:
-    divisor = 3
- 
-for line in sys.stdin:
-    x=int(line)
-    if x % divisor == 0: print(x)
+    DIV = 3
 
-exit()
+for line in sys.stdin:
+    x = int(line)
+    if x % DIV == 0:
+        print(x)
+
+sys.exit()
--- bin/fibonacci.py
+++ bin/fibonacci.py
@@ -2,17 +2,18 @@
 
 import sys
 
-FibArray = [0,1]
+FibArray = [0, 1]
 
 def fibonacci(n):
-    if n<0:
+    if n < 0:
         print("Incorrect input")
-    elif n<=len(FibArray):
+    elif n <= len(FibArray):
         return FibArray[n-1]
     else:
         tmpfib = fibonacci(n-1)+fibonacci(n-2)
         FibArray.append(tmpfib)
         return tmpfib
+
 
 if len(sys.argv) == 2:
     n = int(sys.argv[1]) + 1
@@ -23,7 +24,7 @@ if len(sys.argv) == 3 and sys.argv[1] == "-s":
     n = int(sys.argv[2]) + 1
 
 f = fibonacci(n)
-print("fib(%d) = " % (n-1),end="")
+print("fib(%d) = " % (n-1), end="")
 if arg_s:
     print(f)
 else:
